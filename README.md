a67d5aa2f8f5733158b80eea370f59294200f7ce
dreammarker <minjea456@naver.com>
2020-11-05 16:51:36 +0900

    파이썬 공부 및 문제 풀어보기

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # python_study
 파이썬 공부를 위한 레포
+
+## 파이썬 지원 자료형
+- int : 정수
+- float : 실수
+- complex : 복소수
+- bool : 불린
+- str : 문자열(시퀀스)
+- list : 리스트(시퀸스)
+- tuple : 튜플(시퀀스)
+- set : 집합
+- dice : 사전
---
+++ problem_1.py
@@ -0,0 +1,32 @@
+#  -*- coding: utf-8 -*-
+"""
+문제 설명
+길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+
+제한 조건
+n은 길이 10,000이하인 자연수입니다.
+입출력 예
+n	return
+3	수박수
+4	수박수박
+"""
+
+
+def solution(n):
+    answer = ""
+    for i in range(n): #반복처리
+        if i%2 == 0: #짝수번쨰 인덱스
+            answer += "수"
+        else:# 홀수번쨰 인덱스
+            answer += "박"
+    return answer
+
+
+
+# def solution(n):
+#     s = "수박" * n
+#     return s[:n]
+
+
+result = solution(2)
+print(result)
