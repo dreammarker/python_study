de673d9260f0ff0c483801bedc076df5dffbdb90
dreammarker <minjea456@naver.com>
2020-11-07 23:47:40 +0900

    파이썬 공부

---
+++ practice3.py
@@ -0,0 +1,18 @@
+#  -*- coding: utf-8 -*-
+print(2+3 *4) # 14
+print((2+3)*4) # 20
+number = 2 +3 *4 # 14
+print(number)
+number = number + 2 # 16
+print(number) # 18
+number += 2
+print(number) #20
+number *= 2
+print(number) # 40
+number /= 2
+print(number) # 20
+number -= 2
+print(number) # 18
+
+number %= 5 # 1
+print(number)
---
+++ practice4.py
@@ -0,0 +1,28 @@
+#  -*- coding: utf-8 -*-
+
+print(abs(-5)) # 5
+print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(max(5, 12)) # 12
+print(min(5, 12)) # 5
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+from math import  *
+print(floor(4.99)) # 내림. 4
+print(ceil(3.14)) # 올림 4
+print(sqrt(16)) # 제곱근 4
+
+from random import *
+
+print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+print(random()*10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(random()*10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(random()*10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(random()*10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+
+print(int(random()*45))
+print(int(random()*45))
+print(int(random()*45))
+print(int(random()*45))
+print(int(random()*45))
+print(int(random()*45))
